2c5bf79cceb40b0ce026d7409907fe48763bbf02
José Luis Millán <jmillan@aliax.net>
2018-10-09 13:42:35 +0200

    expose SDP instead of RtpParameters

--- modules/sync_b2bua/module.mk
+++ modules/sync_b2bua/module.mk
@@ -5,6 +5,6 @@
 #
 
 MOD		:= sync_b2bua
-$(MOD)_SRCS	+= sync_b2bua.c sfu_call.c rtp_parameters.c
+$(MOD)_SRCS	+= sync_b2bua.c sfu_call.c
 
 include mk/mod.mk
--- modules/sync_b2bua/sfu_call.h
+++ modules/sync_b2bua/sfu_call.h
@@ -3,12 +3,10 @@
 struct sfu_call;
 
 int sfu_call_alloc(struct sfu_call **callp, const char *id, bool offer);
-int sfu_call_accept(struct sfu_call *call, struct odict *rtp_params, struct odict *rtp_transport);
+int sfu_call_accept(struct sfu_call *call, struct mbuf *desc, bool offer);
 int sfu_call_sdp_debug(const struct sfu_call *call, bool offer);
 int sfu_call_sdp_get(const struct sfu_call *call, struct mbuf **desc, bool offer);
 int sfu_call_sdp_media_debug(const struct sfu_call *call);
-int sfu_call_get_lrtp_parameters(struct sfu_call *call, struct odict **od);
-int sfu_call_get_lrtp_transport(struct sfu_call *call, struct odict **od);
 struct audio *sfu_call_audio(const struct sfu_call *call);
 const char *sfu_call_id(const struct sfu_call *call);
 void sfu_audio_start(const struct sfu_call *call);
